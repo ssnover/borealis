3f566c0ce4238b2244595ef545fc19d1a64bbfb5
ssnover95 <sws7379@rit.edu>
2020-05-09 11:32:59 -0400

    Update execution with timeout from tokio

---
+++ src/discovery.rs
@@ -0,0 +1,37 @@
+use futures_util::{pin_mut, stream::StreamExt};
+use mdns::{Record, RecordKind};
+use std::error::Error;
+use std::net::IpAddr;
+use tokio::time::timeout;
+use tokio::time::Duration;
+
+const NANOLEAF_SERVICE: &'static str = "_nanoleafapi._tcp.local";
+
+pub async fn discover_aurora(query_timeout: Duration) -> Result<IpAddr, Box<dyn Error>> {
+    timeout(query_timeout, get_aurora_ip_address()).await?
+}
+
+async fn get_aurora_ip_address() -> Result<IpAddr, Box<dyn Error>> {
+    let stream = mdns::discover::all(NANOLEAF_SERVICE, Duration::from_secs(5))?.listen();
+    pin_mut!(stream);
+
+    while let Some(Ok(response)) = stream.next().await {
+        println!("Got response to MDNS query.");
+        let addr = response.records().filter_map(self::to_ip_addr).next();
+        if let Some(addr) = addr {
+            return Ok(addr);
+        }
+    }
+    Err(Box::new(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        "Failed to query",
+    )))
+}
+
+fn to_ip_addr(record: &Record) -> Option<IpAddr> {
+    match record.kind {
+        RecordKind::A(addr) => Some(addr.into()),
+        RecordKind::AAAA(addr) => Some(addr.into()),
+        _ => None,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use borealis;
+use borealis::discovery::discover_aurora;
 
 #[tokio::main]
 async fn main() {
     println!("Searching for Nanoleaf Aurora...");
-    match borealis::discover_aurora(std::time::Duration::from_secs(30)).await {
+    match discover_aurora(std::time::Duration::from_secs(30)).await {
         Ok(addr) => {
             println!("Found Nanoleaf Aurora device at {}", addr);
         }
