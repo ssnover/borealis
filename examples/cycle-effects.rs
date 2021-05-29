913af601a07dd7b155b71c98f3509bb0091aca3e
Shane Snover <ssnover95@gmail.com>
2021-05-29 00:44:36 -0600

    Missed some formatting

--- examples/cycle-effects.rs
+++ examples/cycle-effects.rs
@@ -6,16 +6,11 @@ use std::time::Duration;
 pub async fn main() {
     let args: Vec<String> = env::args().collect();
     let cycle_period_secs = args[1].parse::<u64>().expect("Invalid arguments.");
-
     let aurora = Aurora::new("192.168.1.12:16021", "I8NTBbt5IsFhZ5yAuSaa38m9j70m4odx").unwrap();
 
-    tokio::spawn(async move {
-        loop {
-            cycle_effects(&aurora, Duration::from_secs(cycle_period_secs)).await;
-        }
-    })
-    .await
-    .unwrap();
+    loop {
+        cycle_effects(&aurora, Duration::from_secs(cycle_period_secs)).await;
+    }
 }
 
 async fn cycle_effects(aurora: &Aurora<'_>, effect_period: Duration) {
